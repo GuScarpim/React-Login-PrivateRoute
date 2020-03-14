e47c3cbb72e57d9c06b6a849df64c3b679c85271
Gustavo Scarpim <gustavoscarpim@gmail.com>
2020-03-14 14:23:49 -0300

    Inserindo video
    video das funcionalidades do projeto

--- API/server/server.js
+++ API/server/server.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 // const jwt = require('jwt-simple');
 const cors = require('cors');
 
-const mongoURI = 'Sua url do mongo';
+const mongoURI = 'mongodb+srv://GuScarpim:12345@cluster0-edmvi.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true'
+// const mongoURI = 'Sua url do mongo';
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
